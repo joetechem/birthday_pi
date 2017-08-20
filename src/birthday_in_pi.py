c953926fd25f3f164f9a6bb029cc44914fec002a
joetechem <joe@techemstudios.com>
2017-08-20 16:03:01 -0400

    adding first million digits of pi

--- src/birthday_in_pi.py
+++ src/birthday_in_pi.py
@@ -7,11 +7,11 @@ pi_string = r.text
 for rs in r:
     pi_string += rs.strip()
     
-print(pi_string[:32] + "...")
-print(len(pi_string))
+#print(pi_string[:32] + "...")
+#print(len(pi_string))
 
-##birthday = raw_input("Enter your birthday, in the form mmddyy: ")
-##if birthday in r:
-##    print("Your birthday appears in the first million digits of pi!")
-##else:
-##    print("Your birthday does not appear in the first million digits of pi.")
+birthday = raw_input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
